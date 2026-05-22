f106c15e5cad26a326ceeb48f29c1e5a7f0e4efc
Niklas-Nordin <niklas.nordin@chasacademy.se>
2026-05-22 12:34:09 +0200

    Changing zod schema for registration.

--- lib/validations/auth.ts
+++ lib/validations/auth.ts
@@ -1,7 +1,7 @@
 import {z} from "zod";
 
 export const registerSchema = z.object({
-    name: z.string().min(2, "Namnet måste vara minst två tecken långt"),
-    email: z.email("Ogiltig e-postadress"),
+    name: z.string().trim().min(2, "Namnet måste vara minst två tecken långt"),
+    email: z.string().trim().email("Ogiltig e-postadress"),
     password: z.string().min(8, "Lösenordet måste vara minst 8 tecken långt"),
 });
